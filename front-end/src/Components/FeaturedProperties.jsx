f706a83fdda9f11f4302b3c7088e83ea7e84fe31
irorere juliet <irorerejuliet@gmail.com>
2025-06-01 16:16:38 +0100

    make changes on the hero section

--- front-end/src/Components/FeaturedProperties.jsx
+++ front-end/src/Components/FeaturedProperties.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
+import { propertiesData } from '../Constants/propertiesData';
 
 const FeaturedProperties = ({ progress }) => {
   return (
     <div>
       <h2 className="text-5xl font-bold font-Grotesk">Featured Properties</h2>
-      <div className="flex items-center justify-between">
+      <div className="flex items-center justify-between ">
         <p className="text-[1.625rem] w-[806px] text-light">
           Own with peace of mind , these properties are secured by smart
           contracts. No middleman, just transparency and trust
@@ -13,35 +14,37 @@ const FeaturedProperties = ({ progress }) => {
           View all properties
         </button>
       </div>
-      <div>
-        <div className="rounded-xl bg-white w-[587px] mt-16">
-          <img src="/src/assets/Images/house3.svg" alt="house3" />
-          <div className="flex items-center justify-between px-3">
-            <p className="text-[#0A0A23] text-2xl w-[288px] font-medium py-5">
-              150 ETH 5 bedroom detached duplex,osapa lekki lagos
-            </p>
-            <p className="text-[#E5BD3E] text-xl font-bold">
-              12.7% Annual yeild
-            </p>
+      <div className="flex items-center  gap-10">
+        {propertiesData.map(({id, location, profit, title, detials}) => (
+          <div className="rounded-xl bg-white w-[587px] my-16" key={id}>
+            <img src="/src/assets/Images/house3.svg" alt="house3" />
+            <div className="flex items-center justify-between px-3">
+              <p className="text-[#0A0A23] text-2xl w-[288px] font-medium py-5">
+               {location}
+              </p>
+              <p className="text-[#E5BD3E] text-xl font-bold">
+               {profit}
+              </p>
+            </div>
+            <div className="flex justify-between items-center px-4 pt-6">
+              <p className="text-xl text-[#0A0A23] font-bold">
+               {title}
+              </p>
+              <span className="text-[#E5BD3E] text-xl font-bold">50%</span>
+            </div>
+            <div className="w-[560px] h-2 rounded-full bg-[#296B8299] mx-5">
+              <div
+                className="h-full rounded-full bg-[#0A0A23] transition-all duration-300  w-[220px] mt-4 "
+                style={{ width: `${progress}%` }}
+              />
+            </div>
+            <div className=" text-center mx-auto pb-8">
+              <button className="text-xl text-tertiary  font-medium font-inter py-2 px-6 rounded-[20px] mt-10 ml-6  border hover:bg-tertiary  border-tertiary">
+              {detials}
+              </button>
+            </div>
           </div>
-          <div className="flex justify-between items-center px-4 pt-6">
-            <p className="text-xl text-[#0A0A23] font-bold">
-              Tokennized progress:
-            </p>
-            <span className="text-[#E5BD3E] text-xl font-bold">50%</span>
-          </div>
-          <div className="w-[560px] h-2 rounded-full bg-[#296B8299] mx-5">
-            <div
-              className="h-full rounded-full bg-[#0A0A23] transition-all duration-300  w-[220px] mt-4 mx-7"
-              style={{ width: `${progress}%` }}
-            />
-          </div>
-          <div className=" text-center mx-auto pb-8">
-            <button className="text-xl text-tertiary  font-medium font-inter py-2 px-6 rounded-[20px] mt-10 ml-6  border hover:bg-tertiary  border-tertiary">
-              View details
-            </button>
-          </div>
-        </div>
+        ))}
       </div>
     </div>
   );
---
+++ front-end/src/Constants/propertiesData.js
@@ -0,0 +1,16 @@
+export const propertiesData = [
+  {
+    id: 1,
+    location: "150 ETH 5 bedroom detached duplex,osapa lekki lagos",
+    profit: "  12.7% Annual yeild",
+    title: "Tokennized progress:",
+    detials: "View details",
+  },
+  {
+    id: 2,
+    location: "150 ETH 5 bedroom detached duplex,osapa lekki lagos",
+    profit: "  12.7% Annual yeild",
+    title: "Tokennized progress:",
+    detials: "View details",
+  },
+];
