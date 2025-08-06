dc10ba0fab7879954e71b514447db92a66cda0a2
irorere juliet <irorerejuliet@gmail.com>
2025-08-06 07:08:48 +0100

    service profession

--- front-end/src/Components/ProfessionalServiceMainSection.jsx
+++ front-end/src/Components/ProfessionalServiceMainSection.jsx
@@ -8,13 +8,17 @@ const ProfessionalServiceMainSection = () => {
         <h2 className="text-[4rem] font-bold text-secondary font-Grotesk">
           Available Courses
         </h2>
-        <p className="text-2xl font-normal font-inter text-secondary">
-          All verified and rated by the community
-        </p>
+        <div className="flex justify-between items-center">
+          <p className="text-2xl font-normal font-inter text-secondary">
+            All verified and rated by the community
+          </p>
+          <button className="text-xl font-medium py-2 px-8 border border-gray-300 rounded-[20px] ">Sort by: Rating</button>
+        </div>
       </div>
-      <div className="py-10">
+      <div className="grid grid-cols-2 py-10 gap-10">
         {serviceprofessionalData.map(
           ({
+            id,
             image,
             title,
             subTitle,
@@ -23,10 +27,12 @@ const ProfessionalServiceMainSection = () => {
             price,
             chatbox,
             telephone,
-            service
           }) => (
-            <div className="w-[587px] shadow-xl border border-gray-300 rounded-[15px]">
-              <img src={image} alt="divine-construction" />
+            <div
+              className="w-[587px] shadow-xl border border-gray-300 rounded-[15px]"
+              key={id}
+            >
+              <img src={image} alt={title} />
               <div className=" px-6 py-8 space-y-5">
                 <div className="flex justify-between items-center">
                   <h3 className="text-[1.625rem] font-medium text-secondary font-Grotesk">
@@ -37,9 +43,7 @@ const ProfessionalServiceMainSection = () => {
                     <p>{hours}</p>
                   </div>
                 </div>
-                <p className="text-base font-light text-secondary">
-                  {subTitle}
-                </p>
+                <p className="text-xl font-light text-secondary">{subTitle}</p>
                 <p className="text-base font-inter text-secondary">
                   {location}
                 </p>
@@ -52,11 +56,7 @@ const ProfessionalServiceMainSection = () => {
                   </p>
                 </div>
                 <div className="flex gap-5">
-                  <ul className="list-disc ml-5 text-xl font-normal  pt-4 space-y-2 text-[#000000B2]">
-                    {Array.isArray(service) &&
-                      service.map((item, id) => <li key={id}>{item}</li>)}
-                  </ul>
-                  {/* <button className="text-[0.938rem] font-Grotesk font-medium py-1 px-6 border border-gray-300 rounded-[12px] bg-[#F5F8F4]">
+                  <button className="text-[0.938rem] font-Grotesk font-medium py-1 px-6 border border-gray-300 rounded-[12px] bg-[#F5F8F4]">
                     Residential
                   </button>
                   <button className="text-[0.938rem] font-Grotesk font-medium py-1 px-6 border border-gray-300 rounded-[12px] bg-[#F5F8F4]">
@@ -64,10 +64,10 @@ const ProfessionalServiceMainSection = () => {
                   </button>
                   <button className="text-[0.938rem] font-Grotesk font-medium py-1 px-6 border border-gray-300 rounded-[12px] bg-[#F5F8F4]">
                     Residential
-                  </button> */}
+                  </button>
                 </div>
                 <div className="flex justify-between items-center gap-4">
-                  <button className="text-xl font-medium bg-primary text-white py-4 px-20 rounded-[15px]">
+                  <button className="text-xl font-medium bg-primary text-white py-4 px-20 rounded-[15px] mt-2">
                     Hire now
                   </button>
                   <div className="flex items-center gap-4">
@@ -87,6 +87,11 @@ const ProfessionalServiceMainSection = () => {
             </div>
           )
         )}
+      </div>
+      <div className="text-center pt-20">
+        <button className="text-xl font-medium font-Grotesk py-2 px-12  border border-gray-300 rounded-[20px]">
+          View More Services
+        </button>
       </div>
     </div>
   );
--- front-end/src/Pages/ServiceProfessional.jsx
+++ front-end/src/Pages/ServiceProfessional.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 import ProfessionalService from '../Components/ProfessionalServiceHeroSection'
 import ProfessionalServiceHeroSection from '../Components/ProfessionalServiceHeroSection'
 import ProfessionalServiceMainSection from '../Components/ProfessionalServiceMainSection'
+import JionProfessionals from '../Components/JionProfessionals'
 
 const ServiceProfessional = () => {
   return (
     <div>
       <ProfessionalServiceHeroSection/>
       <ProfessionalServiceMainSection/>
+      <JionProfessionals/>
     </div>
   )
 }
