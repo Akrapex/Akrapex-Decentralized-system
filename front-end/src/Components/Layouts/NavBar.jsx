02c2cbd797f19e29037fd9be1a566f9723244f96
Lawrence-afi <84915778+Lawrence-afi@users.noreply.github.com>
2025-08-28 17:06:55 +0100

    i did some changes

--- front-end/src/Components/Layouts/NavBar.jsx
+++ front-end/src/Components/Layouts/NavBar.jsx
@@ -1,4 +1,4 @@
-import { Link, NavLink } from "react-router-dom";
+import { Link, NavLink,useLocation } from "react-router-dom";
 import { IoIosArrowRoundForward } from "react-icons/io";
 
 const NavBar = () => {
