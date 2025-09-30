9113cb6f25124c375abf7f74e31e6f3c2ff1756f
Israel Timothy <125442184+Emitis12@users.noreply.github.com>
2025-09-30 16:23:54 +0100

    Delete frontend directory

--- frontend/src/Constants/propertyTypes.js
+++
@@ -1,8 +0,0 @@
-export const propertyTypes = ["Apartment", "House", "Duplex", "Villa", "Land"];
-export const  Bedrooms =[1,2,3,]
-export const features =["Parking", "Pool", "Gym", "Security", "Garden"];
-export const AISuggestion = [
-  "Properties under 50m in victoria island",
-  "Investment opportunities in Lekki",
-  "Properties under 50m in victoria island",
-];
